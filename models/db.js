fcfc778e01eea5020acd39b701b649298f4e7645
Kiran <kiran0503@gmail.com>
2022-01-17 15:41:36 +0530

    Added functions for CRUD operations with FireStore

--- models/db.js
+++ models/db.js
@@ -1,5 +1,48 @@
 import {db} from "./firebase.js"
-import {collection, addDoc, doc, getDoc, Timestamp, setDoc} from 'firebase/firestore'
+import {collection, doc, getDoc, setDoc, updateDoc, onSnapshot, query, orderBy, limit, deleteDoc} from 'firebase/firestore';
+
+
+// Fetches the data from Firestore with specified Collection Name and DocID 
+async function fetchDataSnap(collectionName, docID){
+    const data = doc(db, collectionName, docID);
+
+    const docSnap = await getDoc(data);
+    return docSnap;
+}
+
+
+// Checks if the data is present in the Collection 
+async function checkData(collectionName, docId){
+    
+    const isDatapresent = await fetchDataSnap(collectionName, docId);
+    if(isDatapresent.exists()){
+        return true;
+    } else{
+        return false;
+    }
+}
+
+// Saves the data in the firestore
+async function saveData(collectionName, docId, data){
+
+    const isDataSaved = setDoc(doc(db, collectionName, docId), data).then(() => {return true}).catch(() => {return false});
+    return isDataSaved;
+}
+
+
+// Updates the data in Firestore Collection
+async function updateData(collectionName, docId, data){
+
+    // Returns true if data updated successfully else false
+    const dataUpdated = await updateDoc(doc(db, collectionName, docId), data).then(() => {
+        return true;
+    }).catch(() => {
+        return false;
+    });
+
+
+    return dataUpdated;
+}
 
 
 // Carries out saving out daily Data of the Dashboard
@@ -12,12 +55,11 @@ export async function saveDailyData(dataObj){
         // Date to be in this format while saving
         // date: new Date(Date.now()).toDateString(),
 
-        const data = doc(db, 'dailyCovidData', date);   
-        const isDatapresent = await getDoc(data);
+        const isDatapresent = await checkData('dailyCovidData', date)
 
-
-        if(isDatapresent.exists()){
-            return false;
+        // Shows that data is already present in DB
+        if(isDatapresent){
+            return !isDatapresent;
         }
 
         else{
@@ -28,11 +70,9 @@ export async function saveDailyData(dataObj){
                 recoveredNum
             }
 
-            const isDataSaved = setDoc(doc(db, 'dailyCovidData', date.toString()), dataToSave).then(() => {return true}).catch(() => {return false});
+            const isDataSaved = await saveData('dailyCovidData', date, dataToSave);
 
-            if(isDataSaved != null){
-                return true;
-            }
+            return isDataSaved;
         }
 
     } catch(e){
@@ -41,45 +81,176 @@ export async function saveDailyData(dataObj){
 
 }
 
-export async function bookAppointment(formData){
-    return true;
+
+
+// Get daily Covid Data 
+export function getDailyCovidData(){
+
+    try {
+        const date = new Date(Date.now()).toDateString();
+        const data = onSnapshot(doc(db, 'dailyCovidData', date), (doc) => {
+            return doc.data();
+        })
+
+    } catch (error) {
+        console.error(error)
+    }
+
 }
 
-export function updateDailyData(data, user){
-    return true;
+
+// Books out appointment of users with their respective userID
+export async function bookAppointment(formData){
+    
+
+    try {
+        // Appointments are fixed with userId as their DocID in 'appointments' collection
+        const {fullName, gender, phoneNum, birthDate, address, email, prevMedHistory, currAilment, user} = formData;
+        
+        // Checks if the appointment is already booked by specified user
+        const isAppointmentPresent = await checkData('appointments', user);
+
+        if(!isAppointmentPresent){
+
+            const dataToSave = {
+                fullName, 
+                gender, 
+                phoneNum, 
+                birthDate,
+                address, 
+                email, 
+                prevMedHistory, 
+                currAilment,
+                date: new Date(Date.now()).toDateString()
+            }
+
+            const isDataSaved = await saveData('appointments', user, dataToSave);
+            return isDataSaved;
+        }
+
+    } catch (error) {   
+        console.error(error.message);
+    }
+
 }
+
+
+
+// Updating the Daily Covid Data by Admin (Health Center Staff)
+export async function updateDailyData(dataToUpdate, user){
+    
+    try {
+
+        const prevDataSnap = await fetchDataSnap('dailyCovidData', dataToUpdate.date);
+        const prevData = prevDataSnap.data();
+
+
+        // Object which stores the data to be updated in firestore collection
+        const dataToSave = {};
+
+
+        // Checks if the value:
+        // 1. Is not NULL
+        // 2. Data to be updated is not previously present value
+        for(let key in dataToUpdate){
+            if(key != 'date' && dataToUpdate[key] != null && dataToUpdate[key] > prevData[key]){
+                dataToSave[key] = dataToUpdate[key];
+            }
+        }
+
+        const dataUpdated = await updateData('dailyCovidData', dataToUpdate.date, dataToSave);
+        return dataUpdated;
+
+
+    } catch (error) {
+        console.error(error.message);
+    }
+
+
+
+}
+
 
 export function receiveAppointments(user){
 
+    try{
+        
+        const q = query(collection(db, 'appointments'), orderBy('date', 'desc'), limit(5));
+        const appointments = [];
+        
+        const unsub = onSnapshot(q, (doc) => {
+            doc.forEach(element => {
+                appointments.push(doc.data());
+            });
+        });
+        
+        return appointments;
+
+    }
+    
+    catch(error){
+        console.error(error);
+    }
+
+
 }
 
-export function cancelAppointment(appointments){
+export async function cancelAppointment(appointment){
+
+    try {
+        
+        const appointmentDeleted = deleteDoc(doc(db, 'appointments', appointment.user)).then(() => {
+            return true;
+        }).catch(() => {
+            return false;
+        });
+        
+        return appointmentDeleted;
+
+
+    } catch (error) {
+        
+    }
 
 }
 
+// Function to perform retrieval of documents from the collection
+function getDocCollection(collectionName, limitNum){
+
+    try {
+        const q = query(collection(db, collectionName), limit(limitNum));
+        const dataArr = [];
+        const unsub = onSnapshot(q, (doc) => {
+            doc.forEach((docData) => {
+                dataArr.push(docData.data());
+            });
+        });
+
+        return dataArr;
+
+    } catch (error) {
+        console.error(error);
+    }
+    
+
+    
+}
+
+
+// Returns the regions for Dashboard map 
 export function getRegionData(){
 
-    // Test Data
-    return [
-        {
-            region: "India",
-            cases: 290002
-        },
 
-        {
-            region: "USA",
-            cases: 21000012
-        },
+    const regions = getDocCollection('regions', 20);
+    return regions;
 
-        {
-            region: "France",
-            cases: 190345
-        },
-    ]
 }
 
 
-export async function signInAdmin(email, password){
+// Returns the healthCentres info 
+export function getHealthCentreInfo(){
 
+    const healthCentres = getDocCollection('healthCentres', 20);
+    return healthCentres;
 }
 
